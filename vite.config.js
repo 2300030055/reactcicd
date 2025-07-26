42e742e748f81dddb1df3ff0783161eeafd6d8fc
2300030055 <navadeepavanigadda@gmail.com>
2025-07-26 22:11:20 +0530

    fix: set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/reactcicd/', // 👈 Add this (it must match your repo name)
   plugins: [react()],
 })
